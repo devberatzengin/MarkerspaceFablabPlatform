5400097c5681488f0701690e36ac33d4f84a75bb
Berat Zengin <devberatzengin@gmail.com>
2026-07-27 13:49:51 +0300

    fix: opened command line

--- Dtos/Equipment/UpdateRequest.cs
+++ Dtos/Equipment/UpdateRequest.cs
@@ -12,7 +12,9 @@ public class UpdateRequest
     public string Name { get; set; } =  string.Empty;
     
     // eğer ben buraya defult değer koyarasm ve istek atarken bu kısma değer vermez isem bu default değer db hye işlenir mo onu denicem bi ara
-    public EquipmentStatus Status { get; set; } = EquipmentStatus.Unknown; 
+    public EquipmentStatus Status { get; set; } = EquipmentStatus.Unknown;
+
+    public TimeSpan UsageTime { get; set; } = TimeSpan.Zero;
     
     [Range(minimum:1, maximum:10,ErrorMessage = "Equipment hard level must be between 1 and 10")]
     public short RequiredUserLevel { get; set; } =  1;
