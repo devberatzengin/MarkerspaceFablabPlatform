a8e00cc3a4238402e6ef3f17714177ca6abdbe81
Berat Zengin <devberatzengin@gmail.com>
2026-07-29 10:04:46 +0300

    feat: new mapper EquipmentRental object to response dto

--- Profiles/EquipmentProfile.cs
+++ Profiles/EquipmentProfile.cs
@@ -12,6 +12,23 @@ public class EquipmentProfile : Profile
             .ForAllMembers(opts =>
                 opts.Condition((src, dest, srcMember) => srcMember != null));
 
-        CreateMap<Equipment, Response>();
+        CreateMap<Equipment, Response>()
+            .ForMember(dest => dest.CurrentUserId,
+                opt => opt.MapFrom(src =>
+                    src.EquipmentRentals
+                        .Where(r => r.ReleasedAt == null)
+                        .Select(r => (Guid?)r.UserId)
+                        .FirstOrDefault()));
+        
+        CreateMap<EquipmentRental, Response>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.EquipmentId))
+            .ForMember(dest => dest.CurrentUserId, opt => opt.MapFrom(src => src.UserId))
+            .ForMember(dest => dest.AvailableAt, opt => opt.MapFrom(src => src.ExpectedReturnAt))
+            .ForMember(dest => dest.Name, opt => opt.Ignore()) // Equipment'tan gelecek
+            .ForMember(dest => dest.Description, opt => opt.Ignore())
+            .ForMember(dest => dest.Type, opt => opt.Ignore())
+            .ForMember(dest => dest.PlacementType, opt => opt.Ignore())
+            .ForMember(dest => dest.Status, opt => opt.Ignore());
+
     }
 }
