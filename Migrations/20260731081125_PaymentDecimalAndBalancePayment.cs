76a33d631a6cdd1744a47e0304c25ada8f4b1c08
Berat Zengin <devberatzengin@gmail.com>
2026-07-31 11:22:54 +0300

    fix: db balance double to decimal

---
+++ Migrations/20260731081125_PaymentDecimalAndBalancePayment.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MakerspaceFablabPlatform.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentDecimalAndBalancePayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaidAmount",
+                table: "Payments");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalAmount",
+                table: "Payments",
+                type: "numeric(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldPrecision: 10,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "RentalFee",
+                table: "Payments",
+                type: "numeric(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldPrecision: 10,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "LateFee",
+                table: "Payments",
+                type: "numeric(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldPrecision: 10,
+                oldScale: 2,
+                oldDefaultValue: 0.0);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DiscountAmount",
+                table: "Payments",
+                type: "numeric(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldPrecision: 10,
+                oldScale: 2,
+                oldDefaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "TotalAmount",
+                table: "Payments",
+                type: "double precision",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(10,2)",
+                oldPrecision: 10,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "RentalFee",
+                table: "Payments",
+                type: "double precision",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(10,2)",
+                oldPrecision: 10,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "LateFee",
+                table: "Payments",
+                type: "double precision",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(10,2)",
+                oldPrecision: 10,
+                oldScale: 2,
+                oldDefaultValue: 0m);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "DiscountAmount",
+                table: "Payments",
+                type: "double precision",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(10,2)",
+                oldPrecision: 10,
+                oldScale: 2,
+                oldDefaultValue: 0m);
+
+            migrationBuilder.AddColumn<double>(
+                name: "PaidAmount",
+                table: "Payments",
+                type: "double precision",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
