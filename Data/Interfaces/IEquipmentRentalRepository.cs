76a0f22024eebbcc3758fc7f77c67e2db3b4d235
Berat Zengin <devberatzengin@gmail.com>
2026-08-05 09:16:08 +0300

    fix: renamefix

--- Data/Interfaces/IEquipmentRentalRepository.cs
+++ Data/Interfaces/IEquipmentRentalRepository.cs
@@ -4,7 +4,7 @@ namespace MakerspaceFablabPlatform.Data.Interfaces;
 
 public interface IEquipmentRentalRepository : IRepository<EquipmentRental>
 {
-    Task<EquipmentRental?> GetCurrentRentalAsync(Guid equipmentId, DateTime asOf, CancellationToken cancellationToken = default);
+    Task<EquipmentRental?> GetCurrentRentalAsync(Guid equipmentId, DateTime beforeOfThisTime, CancellationToken cancellationToken = default);
 
     Task<bool> IsAlreadyTakenThisTimespan(Guid equipmentId, DateTime start, DateTime end, Guid? excludeRentalId = null, CancellationToken cancellationToken = default);
     
